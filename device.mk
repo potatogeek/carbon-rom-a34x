408505e4b9adeada94e1dead9b29cbb2a1b18089
potatogeek <gnogueira3164@gmail.com>
2025-06-17 07:50:50 -0300

    add USB

--- device.mk
+++ device.mk
@@ -149,7 +149,10 @@ PRODUCT_SOONG_NAMESPACES += \
     # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
-
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
