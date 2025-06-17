7ea1c5748ff5b237817aa177111d689fb200ceb5
potatogeek <gnogueira3164@gmail.com>
2025-06-17 07:44:08 -0300

    add more Sensors

--- device.mk
+++ device.mk
@@ -85,7 +85,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
+# Sensors
+PRODUCT_PACKAGES += \
+    -subandroid.hardware.sensors@2 \
+    android.hardware.sensors-service-multihal \
+    android.hardware.sensors@2.0-ScopedWakelock 
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
