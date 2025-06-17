2c15efb1749044daa57ef692d57377dbd4c976f8
potatogeek <gnogueira3164@gmail.com>
2025-06-17 07:49:34 -0300

    add # Thermal

--- device.mk
+++ device.mk
@@ -145,5 +145,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
     hardware/samsung 
+
+    # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/a34x/a34x-vendor.mk)
