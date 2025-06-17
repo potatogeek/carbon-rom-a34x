b7379700ddf5f09e3e95f1ffbdc72f810baaedb0
potatogeek <gnogueira3164@gmail.com>
2025-06-17 07:58:00 -0300

    add fcm

--- device.mk
+++ device.mk
@@ -155,6 +155,11 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget-service.mediatek
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
-
+# fcm
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/compatibility_matrix.xml  \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/a34x/a34x-vendor.mk)
