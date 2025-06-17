2536454f8c07f5f81ec7579ce3b959a84a245fbb
potatogeek <gnogueira3164@gmail.com>
2025-06-17 08:08:15 -0300

    fix build issue

--- carbon_a34x.mk
+++ carbon_a34x.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit from a34x device
 $(call inherit-product, device/samsung/a34x/device.mk)
