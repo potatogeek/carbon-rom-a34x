470ef834d99748e0ebd6ebb1b938d0b35f3f3048
potatogeek <gnogueira3164@gmail.com>
2025-06-17 08:26:30 -0300

    add missing config

--- carbon_a34x.mk
+++ carbon_a34x.mk
@@ -3,7 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
