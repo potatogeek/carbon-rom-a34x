7d12926f3085ef6aec2cdea8b4e396b209a98767
potatogeek <gnogueira3164@gmail.com>
2025-06-17 11:19:21 -0300

    add new build hack

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,8 @@ DEVICE_PATH := device/samsung/a34x
 # build hack
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_DUP_SYSPROP := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
